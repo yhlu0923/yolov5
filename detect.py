e641bff89d131838e659e71d0c890f0097cd253a
yhlu <eric890923@gmail.com>
2022-06-06 21:53:04 +0800

    Change display color in args

--- detect.py
+++ detect.py
@@ -257,7 +257,7 @@ def parse_opt():
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--colors', default={}, type=json, help='')
+    parser.add_argument('--args_colors', default={}, type=json, help='')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
